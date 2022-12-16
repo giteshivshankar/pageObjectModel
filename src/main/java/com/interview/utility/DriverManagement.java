41d5743db729a675af9c6a170b4897a524ede9cb
Shiv <giteshivshankar@gmail.com>
2022-12-16 16:48:48 +0530

    master
    Driver manager added

--- src/main/java/com/interview/utility/DriverManagement.java
+++ src/main/java/com/interview/utility/DriverManagement.java
@@ -4,6 +4,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverManagement {
 	WebDriver driver;
@@ -11,7 +14,8 @@ public class DriverManagement {
 	@SuppressWarnings("deprecation")
 	public WebDriver getDriver(String url) {
 		if(browser.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Drivers/chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Drivers/chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			driver.manage().window().maximize();
@@ -19,8 +23,8 @@ public class DriverManagement {
 			
 			return driver;
 		}else if(browser.equalsIgnoreCase("firefox")) {
-			System.setProperty("webDriver.gecko.driver", System.getProperty("user.dir"+"/geckodriver.exe"));
-			driver = new ChromeDriver();
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
 			driver.manage().window().maximize();
 			return driver;
 		}
